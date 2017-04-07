9aa1991d343e7c32d0c3711977f1ce435f5eba41
Sarvesh Kumar <sarvesh.k@media.net>
2017-04-07 16:29:39 +0530

    Few more

--- TimePass/src/main/java/Trie/trie/Main.java
+++ TimePass/src/main/java/Trie/trie/Main.java
@@ -6,18 +6,17 @@ package Trie.trie;
 public class Main {
     public static void main(String[] args) {
         Trie trie = new Trie();
-        trie.insert("the");
-        trie.insert("there");
+        trie.insert("a");
+        /*trie.insert("there");
         trie.insert("a");
         trie.insert("answer");
         trie.insert("any");
         trie.insert("by");
         trie.insert("bye");
-        trie.insert("their");
+        trie.insert("their");*/
 
-        System.out.println(trie.search("the"));
-        System.out.println(trie.search("these"));
-        System.out.println(trie.search("their"));
-        System.out.println(trie.search("thaw"));
+        System.out.println(trie.searchWithDot(".a"));
+        System.out.println(trie.search(""));
+        System.out.println(trie.search("b"));
     }
 }
--- TimePass/src/main/java/Trie/trie/Trie.java
+++ TimePass/src/main/java/Trie/trie/Trie.java
@@ -10,10 +10,14 @@ public class Trie {
     public Trie() {
     }
 
+    /** Inserts a word into the trie. */
     public void insert(String word) {
         Trie[] tempChar = characters;
         for(int i = 0; i < word.length(); i++) {
             if(tempChar[word.charAt(i) - 97] != null) {
+                if(i == word.length() - 1) {
+                    tempChar[word.charAt(i) - 97].isLeaf = true;
+                }
                 tempChar = tempChar[word.charAt(i) - 97].characters;
             } else {
                 Trie trie = new Trie();
@@ -27,13 +31,51 @@ public class Trie {
         }
     }
 
+    /** Returns if the word is in the trie. */
     public boolean search(String word) {
-        Trie[] tempChar = characters;
+        Trie tempChar = this;
         for(int i = 0; i < word.length(); i++) {
-            if(tempChar[word.charAt(i) - 97] == null) {
+            if(tempChar.characters[word.charAt(i) - 97] == null) {
                 return false;
             }
-            tempChar = tempChar[word.charAt(i) - 97].characters;
+            tempChar = tempChar.characters[word.charAt(i) - 97];
+
+        }
+        return tempChar.isLeaf;
+    }
+
+    /** Returns if the word is in the trie. */
+    public boolean searchWithDot(String word) {
+        Trie tempChar = this;
+        for(int i = 0; i < word.length(); i++) {
+            if(word.charAt(i) != '.') {
+                if(tempChar.characters[word.charAt(i) - 97] == null) {
+                    return false;
+                }
+                tempChar = tempChar.characters[word.charAt(i) - 97];
+            } else {
+                for(int j = 0; j < 26; j++) {
+                    if(tempChar.characters[j] != null) {
+                        if(tempChar.characters[j].searchWithDot(word.substring(i+1, word.length()))) {
+                            return true;
+                        }
+                    }
+                }
+                return false;
+            }
+
+        }
+        return tempChar.isLeaf;
+    }
+
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith(String prefix) {
+        Trie tempChar = this;
+        for(int i = 0; i < prefix.length(); i++) {
+            if(tempChar.characters[prefix.charAt(i) - 97] == null) {
+                return false;
+            }
+            tempChar = tempChar.characters[prefix.charAt(i) - 97];
         }
         return true;
     }
