91a919df9120c103c00e73fd87299b9f694d9e90
apple <apple@MacBook-Air-2.local>
2018-06-06 01:34:35 +0530

    Few more algos

---
+++ TimePass/src/main/java/interview/apple/Main.java
@@ -0,0 +1,10 @@
+package interview.apple;
+
+/**
+ * Created by apple on 04/06/18.
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("apple");
+    }
+}
--- TimePass/src/main/java/iterators/NestedIterator/Main.java
+++ TimePass/src/main/java/iterators/NestedIterator/Main.java
@@ -7,14 +7,14 @@ public class Main {
     public static void main(String[] args) {
         List<NestedInteger> nestedIntegerList = new LinkedList<NestedInteger>();
         nestedIntegerList.add(new NestedInteger(1));
-        nestedIntegerList.add(new NestedInteger(new LinkedList<>(){
+        nestedIntegerList.add(new NestedInteger(new LinkedList<NestedInteger>(){
             {
                 add(new NestedInteger(2));
                 add(new NestedInteger(3));
-                add(new NestedInteger(new LinkedList<>(){
+                add(new NestedInteger(new LinkedList<NestedInteger>(){
                     {
                         add(new NestedInteger(4));
-                        add(new NestedInteger(new LinkedList<>(){
+                        add(new NestedInteger(new LinkedList<NestedInteger>(){
                             {
                                 add(new NestedInteger(7));
                                 add(new NestedInteger(8));
